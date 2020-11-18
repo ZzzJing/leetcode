3591bd68f3e923a9a14ffa7c2574a21d7f339def
jing.zhu <jing.zhu@easystack.cn>
2020-11-19 00:06:05 +0800

    Add dp code

--- dp/322.go
+++ dp/322.go
@@ -1,11 +1,14 @@
 package dp
 
-import "golang.org/x/tools/go/ssa/interp/testdata/src/fmt"
-
 func coinChange(coins []int, amount int) int {
+	if amount == 0 {
+		return 0
+	}
+	big := 999999
 	list := make([]int, amount+1)
 	list[0] = 0
 	for i := 1; i < len(list); i++ {
+		list[i] = big
 		for _, coin := range coins {
 			if i-coin < 0 {
 				continue
@@ -13,8 +16,8 @@ func coinChange(coins []int, amount int) int {
 			list[i] = min(list[i], 1+list[i-coin])
 		}
 	}
-	fmt.Println()
-	if list[amount] != 0 {
+
+	if list[amount] != big {
 		return list[amount]
 	} else {
 		return -1
