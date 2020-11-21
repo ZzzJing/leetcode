cd53df0cbea772b4b2f6fc0d807738200a0227d0
jing.zhu <jing.zhu@easystack.cn>
2020-11-22 00:27:10 +0800

    Add dp code

--- dp/70.go
+++ dp/70.go
@@ -1,12 +1,18 @@
 package dp
 
 func climbStairs(n int) int {
-	if n == 0 {
-		return 0
+	if n < 3 {
+		return n
 	}
 
-	res := make([]int, n+1)
-	for i := 2; i < n; i++ {
-
+	pre := 2
+	ppre := 1
+	var res int
+	for i := 3; i <= n; i++ {
+		res = pre + ppre
+		ppre = pre
+		pre = res
 	}
+
+	return res
 }
--- dp/746.go
+++ dp/746.go
@@ -5,14 +5,16 @@ func minCostClimbingStairs(cost []int) int {
 	if length == 0 || length == 1 {
 		return 0
 	}
-	res := make([]int, length+1)
-	res[0] = 0
-	res[1] = 0
+	res := 0
+	pre := 0
+	ppre := 0
 	for i := 2; i <= length; i++ {
-		res[i] = min(res[i-1]+cost[i-1], res[i-2]+cost[i-2])
+		res = min(pre+cost[i-1], ppre+cost[i-2])
+		ppre = pre
+		pre = res
 	}
 
-	return res[length]
+	return res
 }
 
 func min(p, q int) int {
