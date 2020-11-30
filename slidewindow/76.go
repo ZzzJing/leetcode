2e1306ec2364a25e9fb878cb77e7d359bb9b86d6
jing.zhu <jing.zhu@easystack.cn>
2020-12-01 00:20:57 +0800

    Add slidewinodw code

--- slidewindow/76.go
+++ slidewindow/76.go
@@ -1,5 +1,7 @@
 package slidewindow
 
+import "golang.org/x/tools/go/ssa/interp/testdata/src/fmt"
+
 func minWindow(s string, t string) string {
 	need, window := map[byte]int{}, map[byte]int{}
 	for i := 0; i < len(t); i++ {
@@ -15,21 +17,33 @@ func minWindow(s string, t string) string {
 
 		return true
 	}
-	ansL, ansR := -1, -1
-	left, right := 0, 0
-	for right < len(s) {
+	length := -1
+	ansL := -1
+	for left, right := 0, 0; right < len(s); right++ {
 		if _, exist := window[s[right]]; exist {
 			window[s[right]]++
 		} else {
 			window[s[right]] = 1
 		}
-		right++
-		for check() {
-			ansL = left
-			ansR = right
+		for check() && left <= right {
+			tmpL := right - left + 1
+			if length == -1 {
+				length = tmpL
+				ansL = left
+			} else {
+				if tmpL < length {
+					ansL = left
+					length = tmpL
+				}
+			}
 			window[s[left]]--
 			left++
 		}
-
 	}
+
+	if ansL == -1 {
+		return ""
+	}
+	fmt.Println(ansL, length)
+	return s[ansL : ansL+length]
 }
