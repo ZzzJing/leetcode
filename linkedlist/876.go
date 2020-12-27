3a4bddf27b5c7f5c04d2a121bc2ffe22bd499131
ZzzJing <672427713@qq.com>
2020-12-27 23:34:02 +0800

    Add lks code

---
+++ linkedlist/876.go
@@ -0,0 +1,11 @@
+package linkedlist
+
+func middleNode(head *ListNode) *ListNode {
+	fast , slow := head, head
+	for fast != nil && fast.Next != nil  {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	return slow
+}
